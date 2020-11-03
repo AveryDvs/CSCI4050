2eb6a42d49766a0b5790b057b8c5784eed0c5998
NideshKumar00 <49415476+NideshKumar00@users.noreply.github.com>
2020-11-03 02:58:03 -0500

    Addeed code to Register.java to senc confirmation email

--- java/Register.java
+++ java/Register.java
@@ -2,13 +2,19 @@ import java.io.IOException;
 import java.io.PrintWriter; 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement; 
-  
-import javax.servlet.ServletException; 
-import javax.servlet.annotation.WebServlet; 
-import javax.servlet.http.HttpServlet; 
-import javax.servlet.http.HttpServletRequest; 
-import javax.servlet.http.HttpServletResponse; 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/Register")
 public class Register extends HttpServlet {
@@ -56,6 +62,53 @@ public class Register extends HttpServlet {
             
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        
+        String query = "SELECT email_address, bookstore.user.user_id, cast(aes_decrypt(password, \"4050\") as char) FROM bookstore.customer inner join bookstore.user on bookstore.user.user_id = bookstore.customer.user_id;";
+        boolean valid = false;
+        
+        try {
+        	Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookstore","root","4122");
+			Statement st = con.createStatement();
+			ResultSet rs =st.executeQuery(query);
+			
+			
+			
+		} catch (SQLException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        if (valid) {
+	        String host = "smtp.gmail.com";
+	        String port = "587";
+	        String emailId = "bookstore9c@gmail.com";
+	        String password = "Bookpassword9C";
+	        String toAddress = "bookstore9c@gmail.com";
+	        String subject = "Thank You For Registering With Us!";
+	        String message = "Thank you for regietring with our Bookstore. You now have access to our site!" ;
+	        
+	        try {
+				EmailUtility.sendEmail(host, port, emailId, password, toAddress, subject, message);
+			} catch (AddressException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (MessagingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        out.println("<script>");
+            out.println("alert('Email sent successfully, Check your email');");
+            out.println("</script>");
+        	RequestDispatcher rs = request.getRequestDispatcher("register-confirmation.html");
+            rs.include(request, response);
+        
+        } else {
+        	out.println("<script>");
+            out.println("alert('We do not have any account with provided email, Try again');");
+            out.println("</script>");
+        	RequestDispatcher rs = request.getRequestDispatcher("register-confirmation.html");
+            rs.include(request, response);
         }
         
         
