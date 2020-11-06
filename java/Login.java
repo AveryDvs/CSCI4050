8f15464b610f6dd5f178f20e3378e221866354f7
shreenal59 <sdp68959@uga.edu>
2020-11-05 22:01:41 -0500

    updated login.java that accepts both username and user_id

--- java/Login.java
+++ java/Login.java
@@ -1,9 +1,13 @@
+package com.bookstore;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
 
 import javax.servlet.*;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Login
  */
+@WebServlet("/loginServlet")
 public class Login extends HttpServlet {
 	
 	/**
@@ -23,6 +28,8 @@ public class Login extends HttpServlet {
         String uname = request.getParameter("uname");
         String pass = request.getParameter("psw");
         String query = "select user_id, cast(aes_decrypt(password, \"4050\") as char), User_Type from bookstore.user;";
+        String query2 = "SELECT email_address, cast(aes_decrypt(password, \"4050\") as char), User_Type FROM bookstore.customer inner join bookstore.user on bookstore.user.user_id = bookstore.customer.user_id;";
+        String query3 = "SELECT email_address, bookstore.user.user_id FROM bookstore.customer inner join bookstore.user on bookstore.user.user_id = bookstore.customer.user_id;";
         String userType = "";
         
         boolean valid = false;
@@ -31,6 +38,8 @@ public class Login extends HttpServlet {
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookstore","root","4122");
 			Statement st = con.createStatement();
 			ResultSet rs =st.executeQuery(query);
+			Statement st2 = con.createStatement();
+			ResultSet rs2 = st2.executeQuery(query2);
 			
 			while(rs.next() && !valid) {
 				if (rs.getString(1).equalsIgnoreCase(uname) && rs.getString(2).equals(pass)) {
@@ -38,6 +47,24 @@ public class Login extends HttpServlet {
 					userType = rs.getString(3);
 				}
 			}
+			
+			while(rs2.next() && !valid) {
+				if (rs2.getString(1).equalsIgnoreCase(uname) && rs2.getString(2).equals(pass)) {
+					valid = true;
+					userType = rs2.getString(3);
+					Statement st3 = con.createStatement();
+					ResultSet rs3 = st3.executeQuery(query3);
+					boolean valid1 = false;
+					while(rs3.next() && !valid1) {
+						if (rs3.getString(1).equalsIgnoreCase(uname)) {
+							valid1 = true;
+							uname = rs3.getString(2);
+						}
+					}
+				}
+			}
+			
+			
 			
 		} catch (SQLException | ClassNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -49,6 +76,8 @@ public class Login extends HttpServlet {
         		RequestDispatcher rs = request.getRequestDispatcher("admin.html");
                 rs.include(request, response);
         	} else if (userType.equals("C")) {
+        		Cookie cookie = new Cookie("uname", uname);
+        		response.addCookie(cookie);
         		RequestDispatcher rs = request.getRequestDispatcher("index.html");
                 rs.include(request, response);
         	}
